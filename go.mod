2ca9448b1fd5433929448cad0a9e480aee185233
codeBender31 <58310147+codeBender31@users.noreply.github.com>
2024-03-02 16:54:55 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module ServerProject
+
+go 1.22.0
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+//Import all necessary libraries and useful repos
+
+//To encode and decode JSON objects
+//To encode into base64 format
+//To use for cryptography needed for RSA functionality
+//To be able to assign random numbers
+//For calculations with large ints regarding RSA
+
+//Needed to be able to time out JWTS kid's
+//To check the logs in the console
