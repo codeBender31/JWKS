8dcb24257cdbaa33133cd749eec95ea91756fe0a
CodeBender31 <58310147+codeBender31@users.noreply.github.com>
2024-03-02 17:03:37 -0600

    Update server.go

--- server.go
+++ server.go
@@ -1,12 +1,44 @@
 package main
 
 //Import all necessary libraries and useful repos
+import(
+	"encoding/json" //To encode and decode JSON objects
+	"encoding/base64" //To encode into base64 format
+	"crypto/rsa" //To use for cryptography needed for RSA functionality
+	"crypto/rand" //To be able to assign random numbers
+	"math/big" //For calculations with large ints regarding RSA
+	"net/http" //To set up HTTP connection
+	"time"//Needed to be able to time out JWTS kid's
+	"log" //To check the logs in the console
+	"github.com/golang-jwt/jwt/v4" //Use repo from github to handle JWTs
+	//Reference https://github.com/golang-jwt/jwt
+)
 
-//To encode and decode JSON objects
-//To encode into base64 format
-//To use for cryptography needed for RSA functionality
-//To be able to assign random numbers
-//For calculations with large ints regarding RSA
+//Define struct to hold keys
+type keyPair struct{
+	publicKey *rsa.PublicKey
+	privateKey *rsa.PrivateKey //Two variables to hold rsa key pointers
+	kid 		string
+	expiryTime	time.Time	
+}
 
-//Needed to be able to time out JWTS kid's
-//To check the logs in the console
+//Create a container to hold keyPairs
+var keyPairs []keyPair
+
+//Method to define key pair
+func generateKeys(kid string, numberOfBits int, expiryTime time.Time) keyPair{
+	//Generate the private key
+	newPrivateKey, err := rsa.GenerateKey(rand.Reader, numberOfBits)
+	if err != nil{
+		log.Fatalf("Failure: %v", err)//Show me the error that is generated if it does not work
+	}
+	//Extract public key from the generation command
+	 newPublicKey := &newPrivateKey.PublicKey
+
+	return keyPair{ //return a keyPair object
+		publicKey: newPublicKey,
+		privateKey: newPrivateKey,
+		kid : kid,
+		expiryTime: expiryTime,
+	}
+}
