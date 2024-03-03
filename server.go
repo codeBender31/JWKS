30e9529faaff540b9c99cf4158d1d86e2b5a9ac9
codeBender31 <58310147+codeBender31@users.noreply.github.com>
2024-03-02 19:04:30 -0600

    Fifth commit

--- server.go
+++ server.go
@@ -1,81 +1,150 @@
 package main
 
 //Import all necessary libraries and useful repos
-import(
-	"encoding/json" //To encode and decode JSON objects
+import (
+	"crypto/rand"     //To be able to assign random numbers
+	"crypto/rsa"      //To use for cryptography needed for RSA functionality
 	"encoding/base64" //To encode into base64 format
-	"crypto/rsa" //To use for cryptography needed for RSA functionality
-	"crypto/rand" //To be able to assign random numbers
-	"math/big" //For calculations with large ints regarding RSA
-	"net/http" //To set up HTTP connection
-	"time"//Needed to be able to time out JWTS kid's
-	"log" //To check the logs in the console
+	"encoding/json"   //To encode and decode JSON objects
+	"log"             //To check the logs in the console
+	"math/big"        //For calculations with large ints regarding RSA
+	"net/http"        //To set up HTTP connection
+	"time"            //Needed to be able to time out JWTS kid's
+
 	"github.com/golang-jwt/jwt/v4" //Use repo from github to handle JWTs
 	//Reference https://github.com/golang-jwt/jwt
 )
 
-//Define struct to hold keys
-type keyPair struct{
-	publicKey *rsa.PublicKey
+// Define struct to hold keys
+type keyPair struct {
+	publicKey  *rsa.PublicKey
 	privateKey *rsa.PrivateKey //Two variables to hold rsa key pointers
-	kid 		string
-	expiryTime	time.Time	
+	kid        string
+	expiryTime time.Time
 }
 
-//Create a container to hold keyPairs
+// Create a container to hold keyPairs
 var keyPairs []keyPair
 
-//Method to define key pair
-func generateKeys(kid string, numberOfBits int, expiryTime time.Time) keyPair{
+// Method to define key pair
+func generateKeys(kid string, numberOfBits int, expiryTime time.Time) keyPair {
 	//Generate the private key
 	newPrivateKey, err := rsa.GenerateKey(rand.Reader, numberOfBits)
-	if err != nil{
-		log.Fatalf("Failure: %v", err)//Show me the error that is generated if it does not work
+	if err != nil {
+		log.Fatalf("Failure: %v", err) //Show me the error that is generated if it does not work
 	}
 	//Extract public key from the generation command
-	 newPublicKey := &newPrivateKey.PublicKey
+	newPublicKey := &newPrivateKey.PublicKey
 
 	return keyPair{ //return a keyPair object
-		publicKey: newPublicKey,
+		publicKey:  newPublicKey,
 		privateKey: newPrivateKey,
-		kid : kid,
+		kid:        kid,
 		expiryTime: expiryTime,
 	}
 }
 
-//Create a method to handle the publicKey
-func handlePublicKey (read *http.Request, write http.ResponseWriter){
+// Create a method to handle the publicKey
+func handlePublicKey(write http.ResponseWriter, read *http.Request) {
 	//Define structure for JWKS
-	JWKS := struct{
-		differentKeys []map[string]interface{}'JSON:"keys"'
+	JWKS := struct {
+		Keys []map[string]interface{} `json:"keys"`
 	}{}
 	//Loop through keyPairs
-	for _, i := range keyPairs{
+	for _, i := range keyPairs {
 		//If the current key is not expired compared to current time
-		if i.expiryTime.After(time.Now()){
+		if i.expiryTime.After(time.Now()) {
 			//If pair is current then encode it using base 64
-			n := base64.RawURLEncoding.EncodeToString(keyPair.PublicKey.N.Bytes())
-			e := base64.RawURLEncoding.EncodeToString(big.NewInt(int64(keyPair.PublicKey.E)).Bytes())
+			n := base64.RawURLEncoding.EncodeToString(i.publicKey.N.Bytes())
+			e := base64.RawURLEncoding.EncodeToString(big.NewInt(int64(i.publicKey.E)).Bytes())
 
 			//Build the current JWT with current key pair
 			jwk := map[string]interface{}{
 				//Follow conventional format
 				"kid": i.kid,
-				"algorithm": "RSA256",
+				"alg": "RS256",
 				"kty": "RSA",
 				"use": "sig",
-				"n": n,
-				"e": e,
+				"n":   n,
+				"e":   e,
 			}
 			//Add the encoded jwk into the JWKS
-			JWKS.differentKeys = append(JWKS.differentKeys, jwk)
+			JWKS.Keys = append(JWKS.Keys, jwk)
 		}
 	}
 	//Set the content to JSON
 	write.Header().Set("Content-Type", "application/json")
 	//If an error is encountered
-	if err := json.NewEncoder(write).Encode(JWKS); err != nil{
+	if err := json.NewEncoder(write).Encode(JWKS); err != nil {
 		//Handle error and state the source of problem
 		http.Error(write, "Failure in JWKS", http.StatusInternalServerError)
+		//return back from method
+		return
 	}
+}
+
+// Create a method to handle the POST request to the endpoint
+// As well as generating JWTS
+func authorizationHandler(write http.ResponseWriter, read *http.Request) {
+	//Check if the request is a POST request
+	if read.Method != http.MethodPost {
+		//Let user know it was not allowed
+		http.Error(write, "Not allowed", http.StatusMethodNotAllowed)
+		//Return back from method
+		return
+	}
+	//Declare variables
+	var token string
+	var err error
+	//Check for expired params
+	expired := read.URL.Query().Get("expired") == "true"
+
+	//If length of keypairs container is greater than 0 meaning not empty
+	if len(keyPairs) > 0 {
+		JWTclaims := jwt.MapClaims{
+			"iss": "exampleIssuer",                      //Issuer tag
+			"sub": "exampleSubject",                     //Subject tag
+			"iat": time.Now().Unix(),                    //Issuing time
+			"exp": time.Now().Add(time.Hour * 1).Unix(), // Expiration tag
+		}
+
+		//If expired is a parameter set it to past JWT
+		if expired {
+			JWTclaims["exp"] = time.Now().Add(-time.Hour).Unix()
+		}
+
+		//Create a new JWToken
+		JWToken := jwt.NewWithClaims(jwt.SigningMethodRS256, JWTclaims)
+		//Specify first key's kid
+		JWToken.Header["kid"] = keyPairs[0].kid
+
+		//Turn it into string and sign it
+		token, err = JWToken.SignedString(keyPairs[0].privateKey)
+		//If error is encountered, log that it was unable to be signed
+		if err != nil {
+			http.Error(write, "Failed to be signed", http.StatusInternalServerError)
+			//Return after logging in error
+			return
+		}
+	}
+	//Set the content
+	write.Header().Set("Type", "application/jwt")
+	write.Write([]byte(token)) //Deliver signed JWT
+}
+
+// Create Main function to test all the methods
+func main() {
+	kid := "uniqueExample"
+	expiry := time.Now().Add(24 * time.Hour)
+	keyPair := generateKeys(kid, 2048, expiry)
+
+	keyPairs = append(keyPairs, keyPair)
+
+	http.HandleFunc("/.well-known/jwks.json", handlePublicKey)
+	http.HandleFunc("/auth", authorizationHandler)
+
+	//Let me know if the server start
+	log.Println("Server started at http://localhost:8080")
+	//Start the server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
